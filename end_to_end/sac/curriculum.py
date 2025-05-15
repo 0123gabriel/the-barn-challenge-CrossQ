a6a3adafed86680e3ae03b6c5c6a754b325a026e
bboato <bruno.boato@cern.ch>
2025-05-15 08:24:47 -0300

    fix: :bug: there were two reward type lists

--- end_to_end/sac/curriculum.py
+++ end_to_end/sac/curriculum.py
@@ -155,7 +155,6 @@ class Simple_Curriculum:
         #init_pos_offset = round(random.uniform(-1.5, 1.25), 2) # Random x offset
         
         env_config["kwargs"]["world_name"] = world_name
-        reward_types = ["smooth", "lidar", "simple", "mixed", "local", "simple_local"] 
         env_config["kwargs"]["reward_function"] =reward_types[self.stage] #np.random.choice(reward_types) # "combined"
         env_config["kwargs"]["init_position"] = params["init_position"]
         env_config["kwargs"]["goal_position"] = params["goal_position"]
